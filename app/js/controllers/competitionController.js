867f9604f4f5db5622fbfff3939d923b7cc8e8da
Valentin Nicolas <valentin.ncls@gmail.com>
2017-03-30 16:43:55 +0200

    Super Martin comes to us git add .

--- app/js/controllers/competitionController.js
+++ app/js/controllers/competitionController.js
@@ -1,28 +1,23 @@
 angular.module('app')
-.controller('competitionController', function($scope, competitionService) {
+    .controller('competitionController', function($scope, $timeout, competitionService) {
 
 
         competitionService.getCompetition().then(function(res) {
             $scope.competition = res.data;
             $scope.currentMatchday = res.data.currentMatchday;
             var matchdays = [];
-            for(var day = 1; day <= $scope.competition.numberOfMatchdays; day++) {
-              matchdays.push(day);
+            for (var day = 1; day <= $scope.competition.numberOfMatchdays; day++) {
+                matchdays.push(day);
             }
             $scope.competition.numberOfMatchdays = matchdays;
-    });
-
-    $scope.matchsDay = [];
-
-
-    $scope.$watch('currentMatchday', function (newValue, oldValue) {
-      if(newValue != oldValue) {
-        competitionService.getMatchDay(newValue).then(function(res) {
-            $scope.matchsDay = res.data.fixtures;
-            console.log('coucou');
+            $scope.update();
         });
-      }
-  });
 
+        $scope.matchsDay = [];
+        $scope.update = function() {
+            competitionService.getMatchDay($scope.currentMatchday).then(function(res) {
+                $scope.matchsDay = res.data.fixtures;
 
-});
+            });
+        };
+    });
