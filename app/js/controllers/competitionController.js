d7be6a87be34762ecd412b26fcc5cd32fbb1c1bc
Valentin Nicolas <valentin.ncls@gmail.com>
2017-03-30 15:14:45 +0200

    link dropdown and request

--- app/js/controllers/competitionController.js
+++ app/js/controllers/competitionController.js
@@ -1,7 +1,28 @@
-
 angular.module('app')
-    .controller('competitionController', function($scope, competitionService) {
-        competitionService.getAll().then(function(res) {
-            $scope.schedule = res.data.fixtures;
-        });
+.controller('competitionController', function($scope, competitionService) {
+
+
+        competitionService.getCompetition().then(function(res) {
+            $scope.competition = res.data;
+            $scope.currentMatchday = res.data.currentMatchday;
+            var matchdays = [];
+            for(var day = 1; day <= $scope.competition.numberOfMatchdays; day++) {
+              matchdays.push(day);
+            }
+            $scope.competition.numberOfMatchdays = matchdays;
     });
+
+    $scope.matchsDay = [];
+
+
+    $scope.$watch('currentMatchday', function (newValue, oldValue) {
+      if(newValue != oldValue) {
+        competitionService.getMatchDay(newValue).then(function(res) {
+            $scope.matchsDay = res.data.fixtures;
+            console.log('coucou');
+        });
+      }
+  });
+
+
+});
