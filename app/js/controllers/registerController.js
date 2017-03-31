688ab2777ff7dd39ab5b6f755ac1fea7c9414f23
Emmanuel Jousselin <lafertevidame@gmail.com>
2017-03-31 11:48:13 +0200

    formular almost

--- app/js/controllers/registerController.js
+++ app/js/controllers/registerController.js
@@ -5,18 +5,6 @@ angular.module('app')
                 $state.go('anon.home');
             });
         };
-        $scope.afficher = function() {
-            ApiServices.getAll().then(function(res) {
-                $scope.infoTeams = res.data;
-              });
-          };
-          $scope.afficher();
-
-    $scope.myFunc = function(evt, logo2) {
-                console.log("logo2");
-                $scope.currentId = teams;
-
-            };
 
         // Get the modal
 var modal = document.getElementById('myModal');
