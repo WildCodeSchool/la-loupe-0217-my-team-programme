fa72261c56eb7f0ed0cf1c213cd8a88f6ee4a939
Valentin Nicolas <valentin.ncls@gmail.com>
2017-04-04 17:00:00 +0200

    Big update

--- app/js/controllers/apiController.js
+++ app/js/controllers/apiController.js
@@ -1,16 +1,27 @@
 angular.module('app')
     .controller('ApiController', function($scope, ApiServices) {
-            $scope.afficher = function() {
-                ApiServices.getAll().then(function(res) {
-                    $scope.infoTeams = res.data;
-                });
-            };
-            $scope.afficher();
+        $scope.afficher = function() {
+            ApiServices.getAll().then(function(res) {
+                $scope.infoTeams = res.data;
+                console.log(res.data);
+            });
+        };
+        $scope.afficher();
 
 
-            $scope.currentId = false;
-            $scope.myFunc = function(index) {
-                $scope.currentId = index;
-                  console.log([index]);
-            };
-          });
+        var newArray = [];
+        $scope.currentId = false;
+
+        $scope.myFunc = function(index) {
+            if (newArray.indexOf(index) != -1) {
+                newArray.splice(newArray.indexOf(index), 1);
+            } else {
+                newArray.push(index);
+            }
+
+        };
+
+        $scope.isSelected = function(id) {
+            return newArray.indexOf(id) == -1;
+        };
+    });
